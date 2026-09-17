5fe64b1131fc48c3fb64172bee00c3fa125a8ad1
Luis Alonso <luisalonsoortiz343@gmail.com>
2026-09-16 18:34:14 -0700

    feat: agregar clases entidad (Duenio, Mascota, Veterinario, Consulta)

---
+++ Proyecto_1/src/main/java/Dominio/Entidades/Duenio.java
@@ -0,0 +1,91 @@
+package Dominio.Entidades;
+
+public class Duenio {
+    private int idDueno;
+    private String nombre;
+    private String apellidoP;
+    private String apellidoM;
+    private String direccion;
+    private String telefonos;
+    private String email;
+
+    public Duenio(){
+
+    }
+
+    public Duenio(int idDueno, String nombre, String apellidoP, String apellidoM,
+                 String direccion,  String email) {
+        this.idDueno = idDueno;
+        this.nombre = nombre;
+        this.apellidoP = apellidoP;
+        this.apellidoM = apellidoM;
+        this.direccion = direccion;
+        this.email = email;
+    }
+
+    public Duenio(int idDueno, String nombre, String apellidoP, String apellidoM,
+                  String direccion, String telefonos, String email) {
+        this.idDueno = idDueno;
+        this.nombre = nombre;
+        this.apellidoP = apellidoP;
+        this.apellidoM = apellidoM;
+        this.direccion = direccion;
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getIdDueno() {
+        return idDueno;
+    }
+
+    public void setIdDueno(int idDueno) {
+        this.idDueno = idDueno;
+    }
+
+    public String getApellidoP() {
+        return apellidoP;
+    }
+
+    public void setApellidoP(String apellidoP) {
+        this.apellidoP = apellidoP;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getApellidoM() {
+        return apellidoM;
+    }
+
+    public void setApellidoM(String apellidoM) {
+        this.apellidoM = apellidoM;
+    }
+
+    public String getTelefonos() {
+        return telefonos;
+    }
+
+    public void setTelefonos(String telefonos) {
+        this.telefonos = telefonos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
