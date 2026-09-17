33fa33b19b03130fa022ae1586d8ab31876ddbcc
JorgeU5 <jorge.urbina262725@potros.itson.edu.mx>
2026-09-16 22:06:28 -0700

    Entidades

---
+++ Proyecto_1/src/main/java/Entidades/Duenio.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidades;
+
+/**
+ *
+ * @author ACER
+ */
+public class Duenio {
+    private int idDueno;
+    private String nombre;
+    private String apellidoP;
+    private String apellidoM;
+    private String direccion;
+    private String email;
+
+    public Duenio() {
+    }
+
+    public Duenio(int idDueno, String nombre, String apellidoP, String apellidoM, String direccion, String email) {
+        this.idDueno = idDueno;
+        this.nombre = nombre;
+        this.apellidoP = apellidoP;
+        this.apellidoM = apellidoM;
+        this.direccion = direccion;
+        this.email = email;
+    }
+
+    public int getIdDueno() {
+        return idDueno;
+    }
+
+    public void setIdDueno(int idDueno) {
+        this.idDueno = idDueno;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoP() {
+        return apellidoP;
+    }
+
+    public void setApellidoP(String apellidoP) {
+        this.apellidoP = apellidoP;
+    }
+
+    public String getApellidoM() {
+        return apellidoM;
+    }
+
+    public void setApellidoM(String apellidoM) {
+        this.apellidoM = apellidoM;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
